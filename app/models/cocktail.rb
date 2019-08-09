763088029b1219d2906c1b793bcba48f44b148fe
Nikita Bandarevich <127barcelona@gmail.com>
2019-08-09 17:27:45 +0200

    uploader

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,4 +2,5 @@ class Cocktail < ApplicationRecord
   validates :name, presence: true
   validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }
   has_many :doses
+  mount_uploader :photo, PhotoUploader
 end
